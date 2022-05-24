97ab8d8b487e1008ffdd1ef763269d1e790f2d19
Sanjeev <kumar.sanjeev589@gmail.com>
2022-05-24 16:39:35 +0530

    -d2

---
+++ src/gettingStarted/RotateANumber.java
@@ -0,0 +1,28 @@
+package gettingStarted;
+
+import java.util.Scanner;
+
+public class RotateANumber {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int k = sc.nextInt();
+        int totalDigits =0;
+        int temp = num;
+        while(temp>0){
+            temp/=10;
+            totalDigits++;
+        }
+        k%=totalDigits;  //for k>totalDigits
+        if(k<0)
+            k+=totalDigits; //for -ve value of k
+        int quotient = num/(int)Math.pow(10,k);
+        int remainder = num%(int)Math.pow(10,k);
+
+        int reversedNum = remainder*(int)Math.pow(10,totalDigits-k)+quotient ;
+        System.out.println(reversedNum);
+
+    }
+}
