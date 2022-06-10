92aca03b3e3ad68a41cef858c6a64b55a106ea84
Sanjeev <kumar.sanjeev589@gmail.com>
2022-06-10 08:25:16 +0530

    -d12 09-Jun

---
+++ src/String/ToggleOpticaDocument.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.Scanner;
+
+public class ToggleOpticaDocument {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
+
+    public static String toggleCase(String str) {
+        String s ="";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                char upper = (char) ('A' + c - 'a');
+                s += upper;
+            } else if (c >= 'A' && c <= 'Z') {
+                char lower = (char) ('a' + c - 'A');
+                s += lower;
+            }
+        }
+        return s;
+
+    }
+    }
+
