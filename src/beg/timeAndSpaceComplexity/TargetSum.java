85d123d09d349139c6aa4b00026a78f5f3cae760
Sanjeev <kumar.sanjeev589@gmail.com>
2022-09-07 18:27:25 +0530

    -d23-04 07-Sep

---
+++ src/beg/timeAndSpaceComplexity/TargetSum.java
@@ -0,0 +1,37 @@
+package beg.timeAndSpaceComplexity;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TargetSum {
+
+    public static void targetSumPair(int[] arr, int target){
+        //write your code here
+        Arrays.sort(arr);
+        int i=0,j=arr.length-1;
+        while(i<j){
+            if(arr[i]+arr[j] == target)
+            {
+                System.out.println(arr[i]+", "+arr[j]);
+                i++;
+                j--;
+            }
+            else if(arr[i]+arr[j]>target)
+                j--;
+            else
+                i++;
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int target = scn.nextInt();
+        targetSumPair(arr,target);
+    }
+
+}
