a6a491900e2c4aecee74659f689bc74ae4eb5447
Sanjeev <kumar.sanjeev589@gmail.com>
2022-09-29 14:42:49 +0530

    -d30-01 29-Sep

---
+++ src/beg/linkedList/LinkedListToStackAdapter.java
@@ -0,0 +1,75 @@
+package beg.linkedList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class LinkedListToStackAdapter {
+
+
+    public static class LLToStackAdapter {
+        LinkedList<Integer> list;
+
+        public LLToStackAdapter() {
+            list = new LinkedList<>();
+        }
+
+
+        int size() {
+            return list.size();
+            // write your code here
+        }
+
+        void push(int val) {
+            list.addFirst(val);
+            // write your code here
+        }
+
+        int pop() {
+            if(list.size()==0){
+                System.out.println("Stack Underflow");
+                return -1;
+            }else{
+                return list.removeFirst();
+            }
+            // write your code here
+        }
+
+        int top() {
+
+            if(list.size()==0){
+                System.out.println("Stack Underflow");
+                return -1;
+            }else{
+                return list.getFirst();
+            }
+            // write your code here
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LLToStackAdapter st = new LLToStackAdapter();
+
+        String str = br.readLine();
+        while(str.equals("quit") == false){
+            if(str.startsWith("push")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if(str.startsWith("pop")){
+                int val = st.pop();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            } else if(str.startsWith("top")){
+                int val = st.top();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            } else if(str.startsWith("size")){
+                System.out.println(st.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
